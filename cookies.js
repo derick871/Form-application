18cd84c21f9eebcea3c754fdddaa5fe354258269
derick871 <wanyamaderrick4@gmail.com>
2026-04-11 22:05:52 +0300

    update html,css and js file

--- cookies.js
+++ cookies.js
@@ -1,21 +1,32 @@
-const form= document .querySelector("#form");
-form.addEventListener("submit",function(e){
-    e.preventdefault();
-    const username =
-    document.getElementById("username").value;
-    const email =
-    document.getElementById("email").value;
-    const password =
-    document.getElementById("password").value;
-    const age =
-    document.getElementById("age").value;
+const form = document.getElementById("form");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let age = document.getElementById("age").value;
 
-    document.cookie= `username= ${username};
-    path=/; expires= wed, 22 april ;2026`
-    document.cookie= `email= ${email};
-    path=/; expires= wed, 22 april ;2026`
-    document.cookie= `password= ${password};
-    path=/; expires= wed, 22 april ;2026`
-    document.cookie= `age= ${age};
-    path/; expires= wed, 22 april ;2026`
+    // Reset form
+    form.reset();
+    username = document.getElementById("username").value = "";
+     email = document.getElementById("email").value = "";
+     password = document.getElementById("password").value = "";
+     age = document.getElementById("age").value = "";
+    
+    alert("Form submitted");
+    console.log("Form submitted");
+    
+    // Set cookies
+    document.cookie = `username=${username}; path=/; expires=Wed, 22 Apr 2026 00:00:00 UTC`;
+    document.cookie = `email=${email}; path=/; expires=Wed, 22 Apr 2026 00:00:00 UTC`;
+    document.cookie = `password=${password}; path=/; expires=Wed, 22 Apr 2026 00:00:00 UTC`;
+    document.cookie = `age=${age}; path=/; expires=Wed, 22 Apr 2026 00:00:00 UTC`;
+    
+    
 });
+const button = document.getElementById("btn");
+button.addEventListener("click", function() {
+    form.dispatchEvent(new Event("submit"));
+});
+
